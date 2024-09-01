50aeae885bfb188576f3159951d9d03ae3707dd0
drako9159 <chochito9159@gmail.com>
2024-09-01 15:30:36 -0600

    feat(cors): enable cors

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // cors enable
+  app.enableCors();
+
   await app.listen(4000);
 }
 bootstrap();
